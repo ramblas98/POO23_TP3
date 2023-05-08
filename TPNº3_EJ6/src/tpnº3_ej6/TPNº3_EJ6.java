e49a9195ad62f2bca4cf618f4b90ab6ac0cd684c
Cossio Carina <Cossio Carina@192.168.1.9>
2023-05-08 16:14:47 -0300

    FinalEJ6

--- TPNº3_EJ6/src/tpnº3_ej6/TPNº3_EJ6.java
+++ TPNº3_EJ6/src/tpnº3_ej6/TPNº3_EJ6.java
@@ -4,12 +4,28 @@
  */
 package tpnº3_ej6;
 
+import java.util.Scanner;
+
 
 public class TPNº3_EJ6 {
 
     
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner teclado = new Scanner (System.in);
+        
+        Piso piso = new Piso();
+        
+        Locales local = new Locales();
+        
+       System.out.println("Datos del piso:" + piso);
+       System.out.println("Datos del local:" + local);
+       
+       double precioPiso = piso.Precio();
+       System.out.println("Precio total del piso: " + precioPiso);
+       
+       double precioLocal = local.Precio();
+       System.out.println("Precio total del piso: " + precioLocal);
+       
     }
     
 }
