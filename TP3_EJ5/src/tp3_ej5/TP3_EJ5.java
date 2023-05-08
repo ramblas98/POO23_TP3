2917c14110ccd55a04bdd79c5562b2938125ec9c
David <David@LAPTOP-887LF5BD>
2023-05-08 00:42:27 -0300

    ej3.6

--- TP3_EJ5/src/tp3_ej5/TP3_EJ5.java
+++ TP3_EJ5/src/tp3_ej5/TP3_EJ5.java
@@ -14,7 +14,7 @@ public class TP3_EJ5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
     }
     
 }
